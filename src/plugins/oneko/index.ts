9bb9c88e6d9e53e39d12401ced2afda0bbe6a108
thororen <78185467+thororen1234@users.noreply.github.com>
2024-01-10 15:15:09 -0500

    Remove Coin

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -55,6 +55,10 @@ export default definePlugin({
     },
 
     stop() {
+        const coinsEnabled = settings.store.coinsEnabled;
         document.getElementById("oneko")?.remove();
+        if (coinsEnabled) {
+            document.getElementById("coin")?.remove();
+        }
     }
 });
